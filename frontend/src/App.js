9c80ae58ea0b12c89760d7050ec6fede8caff8fb
Tiago Carlos <tiago.carlos1@estudante.ufla.br>
2022-08-11 14:20:56 -0300

    Deletar e Completar finalizados

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,72 +1,121 @@
 import 'bootswatch/dist/lux/bootstrap.css';
 import React, { Component, useEffect, useState } from 'react';
 import ReactPaginate from 'react-paginate';
+import axios from 'axios';
 
-function App () {
+function App() {
 
   const [items, setItems] = useState([])
   const [pageCount, setPageCount] = useState(0);
+  const [currentPage, setCurrentPage] = useState(0);
   let page_size = 6;
 
-  useEffect( () => {
-    const getTarefas = async () => {
-      const res = await fetch(
-        `http://localhost:8080/tarefas?page=0&size=${page_size}`
-      );
-      const data = await res.json();
-      console.log(data.content)
-      setItems(data.content);
-      setPageCount(data.totalPages)
-    };
-
-    getTarefas();
+  useEffect(() => {
+    axios.get(`http://localhost:8080/tarefas?page=0&size=${page_size}&sort=id`)
+      .then(res => {
+        const data = res.data;
+        setItems(data.content);
+        setPageCount(data.totalPages)
+      });
   }, []);
 
-  //console.log(items);
+  // Função responsável por atualizar a página, e mante-lá em uma variável global
+  // Quando o valor de currentPage é alterado, essa função roda
+  useEffect(() => {
+    console.log(currentPage)
+    fetchTarefas();
+  }, [currentPage]);
 
-  const fetchTarefas = async (currentPage) => {
+  useEffect(() => {
+    if (currentPage !== 0) {
+      setCurrentPage(currentPage - 1)
+    }
+  }, [pageCount]);
+
+  const completeTarefa = async (tarefa) => {
+    tarefa.completa = !tarefa.completa;
+    console.log(tarefa)
+    axios.put(`http://localhost:8080/tarefas/${tarefa.id}`, tarefa)
+      .then(res => {
+        return fetchTarefas();
+      });
+  }
+
+  const deleteTarefa = async (tarefa) => {
+    axios.delete(`http://localhost:8080/tarefas/${tarefa.id}`)
+      .then(res => {
+        return fetchTarefas();
+      });
+  }
+
+  const fetchTarefas = async () => {
+    console.log(currentPage)
     const res = await fetch(
-      `http://localhost:8080/tarefas?page=${currentPage}&size=${page_size}`
+      `http://localhost:8080/tarefas?page=${currentPage}&size=${page_size}&sort=id`
     );
     const data = await res.json();
-
-    return data.content
+    if (pageCount != data.totalPages) { console.log(pageCount + " - " + data.totalPages); setPageCount(data.totalPages) }
+    console.log(data.content.sort((a, b) => a.id - b.id));
+    setItems(data.content)
   }
 
   const handlePageClick = async (data) => {
-    let currentPage = data.selected;
-    const tarefasFormServer = await fetchTarefas(currentPage);
-
-    setItems(tarefasFormServer)
+    setCurrentPage(data.selected)
   }
 
   return (
     <div className='container'>
       <div className='row m-2'>
-              <button type="button" className="btn btn-success">Adicionar Tarefa</button>
+        <div className='card text-white bg-dark text-center'>
+          <div className='card-header'>
+            Adicionar nova tarefa
+          </div>
+          <div className='card-body'>
+            <div class="form-group">
+              <fieldset>
+                <input class="form-control text-center" id="readOnlyInput" type="text" placeholder="ID" readonly="" />
+              </fieldset>
+            </div>
+
+            <div class="form-group">
+              <fieldset>
+                <label class="form-label" for="disabledInput">Descricao</label>
+                <input type="text" className="form-control text-center" placeholder="descricao" id="inputDefault" />
+              </fieldset>
+            </div>
+            <div class="form-group">
+              <fieldset>
+                <label class="form-label" for="disabledInput">Prazo</label>
+                <input type="text" className="form-control text-center" placeholder="dd-MM-yyyy HH:mm" id="inputDefault" />
+                <small id="emailHelp" class="form-text text-muted">Não esqueça do espaço entre a data e a hora.</small>
+              </fieldset>
+            </div>
+            <button type="submit" className="btn btn-success">Adicionar Tarefa</button>
+          </div>
+        </div>
         {items.map((item) => {
           let card_type = "card text-white bg-dark text-center"
           let button_text = "completar"
           let button_type = "btn btn-success"
-          if (item.completa === true) { 
+          if (item.completa === true) {
             card_type = "card text-white bg-success text-center"
-            button_text = "descompletar"
+            button_text = "resetar"
             button_type = "btn btn-warning"
           }
-          var options = { year: 'numeric', month: 'short', day: 'numeric'};
-          const data = new Date(item.prazo).toLocaleDateString([],options);
-          const hora = new Date(item.prazo).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+          var options = { year: 'numeric', month: 'short', day: 'numeric' };
+          const data = new Date(item.prazo).toLocaleDateString([], options);
+          const hora = new Date(item.prazo).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
           return (
             <div key={item.id} className='col-sm-6 col-md-4 v my-2'>
-              <div className={card_type} style={{minHeight: 225}}>
+              <div className={card_type} style={{ minHeight: 225 }}>
                 <div className='card-header'>
                   <p className='card-text'>{data} - {hora}</p>
                 </div>
                 <div className='card-body'>
                   <h5 className="card-title text-center h2">{item.descricao}</h5>
-                  <button type="button" className="btn btn-secondary" onClick={ () => console.log("id: " + item.id)}>Editar</button>
-                  <button type="button" className="btn btn-danger">Deletar</button>
-                  <button type="button" className={button_type}>{button_text}</button>
+                  <button type="button" className="btn btn-secondary" onClick={() => console.log(currentPage)}>Editar</button>
+                  <button type="button" className="btn btn-danger" onClick={() => deleteTarefa(item)}>Deletar</button>
+                  <button type="button" className={button_type} onClick={() => completeTarefa(item)}>{button_text}</button>
                 </div>
               </div>
             </div>
@@ -74,7 +123,7 @@ function App () {
         })}
       </div>
 
-      <ReactPaginate 
+      <ReactPaginate
         previousLabel={""}
         nextLabel={""}
         breakLabel={"..."}
@@ -90,8 +139,8 @@ function App () {
         activeClassName={"active"}
       />
     </div>
-    
-  ) 
+
+  )
 }
 
 export default App;
