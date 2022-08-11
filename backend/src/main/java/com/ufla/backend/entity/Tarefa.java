25959f3bcba61edec5567ffd5fdd52e4740b62e2
Tiago Carlos <tiago.carlos1@estudante.ufla.br>
2022-08-11 14:19:27 -0300

    Alterado formato esperado da data

--- backend/src/main/java/com/ufla/backend/entity/Tarefa.java
+++ backend/src/main/java/com/ufla/backend/entity/Tarefa.java
@@ -23,7 +23,7 @@ public class Tarefa {
     private String descricao;
 
     @Column(name = "prazo")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="dd-MM-yyyy HH:mm")
     private Date prazo;
 
     @Column(name = "completa")
